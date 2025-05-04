2cd58eec7fa2d769fa3b076d7e7195c78f27ef11
Layne Magnuson <67352060+laynemag@users.noreply.github.com>
2025-05-03 22:38:26 -0500

    more actions changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://somfitness.co
 export default defineConfig({
-    site: 'https://laynemag.github.io'
+    site: 'http://somfitness.co',
 });
